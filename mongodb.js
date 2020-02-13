e3d63bd163dedb453c2211065d6a077f22fb38ec
Sergei Komarov <komarovs33@mail.ru>
2020-02-13 09:31:15 +0300

    insert many

--- mongodb.js
+++ mongodb.js
@@ -14,9 +14,62 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Sergei",
-      age: 29,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Sergei",
+    //     age: 29,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert record");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jan",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Lisa",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable insert message");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "first goal",
+          completed: true,
+        },
+        {
+          description: "second goal",
+          completed: false,
+        },
+        {
+          description: "third goal",
+          completed: true,
+        },
+      ],
+      (err, result) => {
+        if (err) {
+          return console.log("Unable to insert docs");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
